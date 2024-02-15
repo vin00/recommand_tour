cf159992f9910ed49638b4340cf67f0c1533554d
vin00 <155034374+vin00@users.noreply.github.com>
2024-02-15 23:09:49 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,9 +1,7 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     //홈페이지 로드시 팝업 보이기
     showPopup();
 });
-
 
 function showPopup() {
     document.getElementById('contPopup').style.display = 'flex';
@@ -12,7 +10,7 @@ function showPopup() {
 function closePopup() {
     document.getElementById('contPopup').style.display = 'none';
 }
-// DOM 요소들을 변수에 할당
+
 const $objbutton = document.querySelector('.obj-button');
 const $objai = document.querySelector('.obj-txt-ai');
 const $lablRegion = document.getElementById('lablRegion');
